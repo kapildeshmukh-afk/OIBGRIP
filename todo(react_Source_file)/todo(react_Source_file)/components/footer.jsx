a177ad6db7399dd2226f2da6ed8a9067ee567c64
kapildeshmukh-afk <80241134+kapildeshmukh-afk@users.noreply.github.com>
2022-08-30 23:27:45 +0530

    Delete todo(react_Source_file)/todo(react_Source_file) directory

--- todo(react_Source_file)/todo(react_Source_file)/components/footer.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import './styles.css'
-function Footer(){
-    return <footer>
-        <p> copyright &#169; {new Date().getFullYear()}</p>
-    </footer>
-}
-export default Footer;
