dd9af85e14b0cfa8923fde6a95fa500b02867c7d
kapildeshmukh-afk <80241134+kapildeshmukh-afk@users.noreply.github.com>
2022-08-30 23:24:37 +0530

    Add files via upload

---
+++ todo(react_Source_file)/components/footer.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import './styles.css'
+function Footer(){
+    return <footer>
+        <p> copyright &#169; {new Date().getFullYear()}</p>
+    </footer>
+}
+export default Footer;
